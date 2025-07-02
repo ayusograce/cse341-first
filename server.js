c70a5170bbf1dd0ebabad12c12b23102739f3234
Grace Ayuso <adriayusof@gmail.com>
2025-07-02 14:56:24 -0500

    creating server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Example route
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the API!' });
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
